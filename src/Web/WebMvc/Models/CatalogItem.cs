7b1e5143baa20acd8a5abd36e180c06271670629
aires.diego66 <aires.diego66@outlook.com>
2019-04-02 19:38:38 -0300

    Adição cliente consome Microserviços

---
+++ src/Web/WebMvc/Models/CatalogItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoesOnContainer.Web.WebMvc.Models
+{
+    public class CatalogItem
+    {
+        //Foi decidido usar uma classe só para as 3 entidades do banco de dados
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string PictureUrl { get; set; }
+
+        public int CatalogBrandId { get; set; }
+        public string CatalogBrand { get; set; }
+
+        public int CatalogTypeId { get; set; }
+        public string CatalogType { get; set; }
+
+    }
+}
